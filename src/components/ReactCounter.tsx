8ae1b2a82ad3419c9d5d83fd8bf80a2e5de1759e
Richard Miller <richardmmill@gmail.com>
2022-01-12 20:41:29 -0500

    setup, config, understanding

--- src/components/ReactCounter.tsx
+++ src/components/ReactCounter.tsx
@@ -3,8 +3,12 @@ import { useState } from 'react';
 
 const ReactCounter = (): JSX.Element => {
   const [count, setCount] = useState<number>(0);
-  const add = () => setCount((i) => i + 1);
-  const subtract = () => setCount((i) => i - 1);
+  const add = () => {
+    setCount((i) => i + 1)
+  };
+  const subtract = () => {
+    setCount((i) => i - 1)
+  };
 
   return (
     <div id="react" className="counter">

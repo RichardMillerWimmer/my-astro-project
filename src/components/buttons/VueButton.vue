b413b0c5372b4337463e689662310957112d596c
Richard Miller <richardmmill@gmail.com>
2022-01-16 17:04:50 -0500

    add vue button component

---
+++ src/components/buttons/VueButton.vue
@@ -0,0 +1,20 @@
+<template>
+    <button @click="vueAlert">Vue</button>
+</template>
+
+<script>
+export default {
+    setup() {
+        
+    },
+    methods: {
+        vueAlert() {
+            alert('This is the Vue component.')
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
